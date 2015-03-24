04c1f8bf3ebca8eb431ccba91e6d3fd278fec858
Tony Paek <paek.min@gmail.com>
2015-03-24 16:58:38 -0400

    Commit
    update read.me to include links, remove unnecessary files

--- uni_rank_stat/global.R
+++
@@ -1,8 +0,0 @@
-# Variables that can be put on the x and y axes
-axis_vars <- c(
-  "University Ranking" = "Rank",
-  "Admission Rate" = "Acceptance_Rate",
-  "Graduation Rate" = "Graduation_Rate",
-  "4 Year Tuition Cost" = "Total_Cost",
-  "20YR Aggregate Return" = "Return_Value"
-)
